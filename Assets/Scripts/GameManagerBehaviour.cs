ecf99256bfee6a964597b6ec731d424816477fa1
Stephen <stephenmccaskie05@outlook.com>
2025-12-03 14:19:07 +0000

    Ui updated

---
+++ Assets/Scripts/GameManagerBehaviour.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameManagerBehaviour : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public static bool isGamePaused;
+
+  
+    bool prevCursorVisible;
+    CursorLockMode prevLockState;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        isGamePaused = false;
+
+        prevCursorVisible = Cursor.visible;
+        prevLockState = Cursor.lockState;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!isGamePaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+
+
+        prevCursorVisible = Cursor.visible;
+        prevLockState = Cursor.lockState;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+     
+        if (EventSystem.current != null)
+            EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+
+        Cursor.visible = prevCursorVisible;
+        Cursor.lockState = prevLockState;
+    }
+}
---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void MainMenuScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
