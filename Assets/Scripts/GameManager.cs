5ecc293118cf3ef9231fb2aa7aa54df5f3b27ab6
Stephen <stephenmccaskie05@outlook.com>
2025-12-09 07:13:02 +0000

    final changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,17 +5,10 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    //[SerializeField]GameObject pauseMenu;
     [SerializeField]GameObject winMenu;
 
     UIMananger uiManager;
     
-
-    public static bool isGamePaused;
-
-    bool prevCursorVisible;
-    CursorLockMode prevLockState;
-
     [Header("Item Collection")]
     [SerializeField] public int itemsNeeded = 5;
     [SerializeField] public int itemsCollected = 0;
@@ -37,61 +30,16 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        /*pauseMenu.SetActive(false);
-        isGamePaused = false;
-
-        prevCursorVisible = Cursor.visible;
-        prevLockState = Cursor.lockState; */
-
         UpdateCollectionUI();
     }
 
     void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            if (!isGamePaused)
-            {
-                PauseGame();
-            }
-            else
-            {
-                ResumeGame();
-            }
-        } */
-
         if (itemsCollected >= 4)
         {
             itemsProgressText.color = Color.yellow;
         }
     }
-
-    /*public void PauseGame()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        isGamePaused = true;
-
-
-        prevCursorVisible = Cursor.visible;
-        prevLockState = Cursor.lockState;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
-
-     
-        if (EventSystem.current != null)
-            EventSystem.current.SetSelectedGameObject(null);
-    }
-
-    public void ResumeGame()
-    {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        isGamePaused = false;
-
-        Cursor.visible = prevCursorVisible;
-        Cursor.lockState = prevLockState;
-    } */
 
     public void CollectItem(int amount = 1)
     {
